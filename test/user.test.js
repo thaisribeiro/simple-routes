f665ad7e9c2dbb3236f0817bd3322d0ce9c19734
Your Namethaisribeiro <thaisnribeiro2008@gmail.com>
2019-01-18 17:49:43 -0200

    feat(arquitetura): criação de testes jest

--- test/user.test.js
+++ test/user.test.js
@@ -1,16 +1,53 @@
 import 'babel-polyfill'
 import request from 'supertest'
 import es from '../app'
+import faker from 'faker/locale/pt_BR'
 
 const app = es.express
 
-describe('POST create User', () => {
-  test('Should list the user and return success', (done) => {
-    return request(app).get('/users')
-      .expect(200)
+describe('POST /user', () => {
+  test('Should create the user and return success', (done) => {
+    const user = {
+      name: faker.name.findName(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      address: faker.address.streetAddress()
+    }
+
+    return request(app)
+      .post('/users')
+      .send(user)
       .then((response) => {
-        expect(response.body).toBe(200)
+        expect(response.statusCode).toBe(200)
         done()
       })
+      .catch(error => done(error))
   })
+
+  // test('Should create the user and return success', () => {
+  //   const user = {
+  //     name: faker.name,
+  //     email: faker.email,
+  //     address: faker.address
+  //   }
+
+  //   return request(app)
+  //     .post('/users')
+  //     .send(user)
+  //     .expect(400)
+  //     .then((response) => {
+  //       expect(response.statusCode).toBe(400)
+  //     })
+  // })
 })
+
+// describe('GET /user', () => {
+//   test('Should list the user and return success', () => {
+//     return request(app)
+//       .get('/users')
+//       .expect(200)
+//       .then((response) => {
+//         expect(response.statusCode).toBe(200)
+//       })
+//   })
+// })
